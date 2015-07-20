f79932258556eb84f72a1d2b3dba934fb28054b9
Eloy Pineda <me@eloy.codes>
2015-07-20 23:31:33 +0200

    chore(gulpfile): add babel and babelify packages to transpile ES6 JS
    rename to gulp file to `gulpfile.babel.js` to be able to use ES6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,3 @@
-'use strict';
 
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
@@ -34,31 +33,31 @@ gulp.task('styles', function () {
 });
 
 gulp.task('browserify', function() {
-	// reactify+watchify+browserify
+	// babelify+watchify+browserify
 
   var bundler = browserify({
     entries: ['./src/scripts/index.js'], // Only need initial file, browserify finds the deps
-    transform: [reactify], // Convert JSX to normal javascript
+    transform: [babelify], // Convert JSX to normal javascript
     debug: true, cache: {}, packageCache: {}, fullPaths: true
   });
 
   var watcher  = watchify(bundler);
 
   return watcher
-	  .on('update', function () { // When any files updates
-	      var updateStart = Date.now();
-				plugins.util.log('Updating!');
-	      watcher.bundle()
-	      .pipe(source('index.js'))
-	      // This is where you add uglifying etc.
-	      .pipe(gulp.dest('./build/scripts/'))
-				.pipe(reload({stream: true}));
-				plugins.util.log('Updated!', (Date.now() - updateStart) + 'ms');
-	  })
-	  .bundle() // Create the initial bundle when starting the task
-	  .pipe(source('index.js'))
-	  .pipe(gulp.dest('./build/scripts/'))
-		.pipe(reload({stream: true}));
+    .on('update', function () { // When any files updates
+      var updateStart = Date.now();
+      plugins.util.log('Updating!');
+      watcher.bundle()
+      .pipe(source('index.js'))
+      // This is where you add uglifying etc.
+      .pipe(gulp.dest('./build/scripts/'))
+      .pipe(reload({stream: true}));
+      plugins.util.log('Updated!', (Date.now() - updateStart) + 'ms');
+    })
+    .bundle() // Create the initial bundle when starting the task
+    .pipe(source('index.js'))
+    .pipe(gulp.dest('./build/scripts/'))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('serve', function () {
