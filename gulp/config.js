39f38b9bf56d95aeba600d9f51090ef12f1e730c
Eloy Pineda <me@eloy.codes>
2015-11-13 01:04:00 +0100

    style(gulp): config main properties sorted alphabetically

--- gulp/config.js
+++ gulp/config.js
@@ -4,19 +4,24 @@ const DEST = './build';
 const SRC = './src';
 
 export default {
-  serve: {
-    server: { baseDir: DEST },
-    notify: false
-  },
-  markup: {
-    src: SRC + '/*.html',
-    dest: DEST
-  },
   assets: {
     images: {
       src: SRC + '/assets/images/*.{svg,png,jpg,gif}',
       dest: DEST + '/images'
     }
+  },
+  browserify: {
+    src: SRC + '/scripts/index.js',  // Only need initial file, browserify finds the deps
+    outputName: 'index.js',
+    dest: DEST + '/scripts'
+  },
+  markup: {
+    src: SRC + '/*.html',
+    dest: DEST
+  },
+  serve: {
+    server: { baseDir: DEST },
+    notify: false
   },
   styles: {
     src: SRC + '/styles/**/*.{sass,scss}',
@@ -28,10 +33,5 @@ export default {
         'node_modules/font-awesome/scss/'
       ]
     }
-  },
-  browserify: {
-    src: SRC + '/scripts/index.js',  // Only need initial file, browserify finds the deps
-    outputName: 'index.js',
-    dest: DEST + '/scripts'
   }
 };
