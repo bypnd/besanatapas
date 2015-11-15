05a268da6a3a609247f9d726bc65e4836ed18519
Eloy Pineda <me@eloy.codes>
2015-11-15 20:50:47 +0100

    fix: import React in `index.js` to process JSX inside `ReactDOM.render` function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
+import React from 'react'; //eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
 import Timeline from './components/timeline.react';
 import TimelineSources from './utils/timeline-apis';
