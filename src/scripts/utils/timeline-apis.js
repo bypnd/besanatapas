796041cf9cf81852ba1492cb7d8d2aff7159b76c
Eloy Pineda <me@eloy.codes>
2016-01-18 23:15:45 +0100

    chore(config): add twitter/instagram API settings to config files

--- src/scripts/utils/timeline-apis.js
+++ src/scripts/utils/timeline-apis.js
@@ -1,23 +1,20 @@
+import config from '../config'
 import jsonp from 'jsonp'
 import logger from '../utils/logger'
 import request from 'superagent'
 
 import TimelineActions from '../actions/timeline-actions'
 
-const instagram = {
-  userId: '1464295575',
-  clientId: 'c0901148858d4ec684d3894bc6c7bc1c'
-}
+const instagramUserId = config.instagram.userId
+const instagramClientId = config.instagram.clientId
 
-const twitter = {
-  userId: '239744001'
-}
+const twitterURL = config.twitter.apiUrl
 
 export default {
   fetchAPI: function () {
     // fetch instagram API
     jsonp(
-      'https://api.instagram.com/v1/users/' + instagram.userId + '/media/recent/?client_id=' + instagram.clientId,
+      `https://api.instagram.com/v1/users/${instagramUserId}/media/recent/?client_id=${instagramClientId}`,
       function (err, res) {
         if (err) {
           logger.error('Ajax error!', err.toString())
@@ -29,8 +26,8 @@ export default {
     )
     // fetch twitter API
     request
-      .get('http://localhost:5000/statuses/user_timeline.json')
-      .query({ user_id: twitter.userId })
+      .get(`${twitterURL}/statuses/user_timeline.json`)
+      .query({ user_id: config.twitter.userId })
       .end(function (err, res) {
         if (err) {
           logger.error('Ajax error!', err.toString())
